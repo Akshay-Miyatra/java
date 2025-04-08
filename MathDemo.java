0ef14c79628e296620126d75ae562e26d9a04d4c
Akshay-Miyatra <mrakmiyatra@gmail.com>
2025-04-08 10:40:13 +0530

    Add files via upload

---
+++ MathDemo.java
@@ -0,0 +1,13 @@
+package MathPack;
+
+public class MathDemo {
+    // Method to add two numbers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method to subtract two numbers
+    public int sub(int a, int b) {
+        return a - b;
+    }
+}
